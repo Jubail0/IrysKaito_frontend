7fb503444a55bbd8e0a71a772190ad9914a11032
Jubail Mallick <98705208+Jubail0@users.noreply.github.com>
2025-08-17 17:37:10 +0530

    Update IrysGallery.jsx

--- src/Pages/IrysGallery.jsx
+++ src/Pages/IrysGallery.jsx
@@ -5,7 +5,7 @@ import GalleryCard from '../Components/Gallery/GalleryCard.jsx';
 import Filteration from '../Components/Gallery/Filteration.jsx';
 import { gsap } from 'gsap';
 
-const IrysGallery = ({connected, username,address }) => {
+const IrysGallery = ({connected, username,address, xUsername }) => {
   const [profiles, setProfiles] = useState([]);
   const [nodes, setNodes ] = useState([]);
   const imgRef = useRef(null);
@@ -98,7 +98,7 @@ useEffect(() => {
     <Filteration
       profiles={profiles}
       setProfiles={setProfiles}
-      username={username}
+      username={xUsername}
     />
   </div>}
 </div>
