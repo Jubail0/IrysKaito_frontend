21fe44b82e66101a070207480694f4cf885476e8
Jubail Mallick <98705208+Jubail0@users.noreply.github.com>
2025-09-14 14:24:05 +0530

    Add files via upload

---
+++ src/Auth/auth.js
@@ -0,0 +1,43 @@
+export const authentication = async (setConnected, setAuthUsername, setAddress) => {
+ 
+  try {
+    const token = localStorage.getItem("userJWT");
+    if (!token) return;
+
+    const payload = JSON.parse(atob(token.split(".")[1]));
+     if (payload.username) setAuthUsername(payload.username);
+     
+     if (payload.walletAddress) {
+     setAddress(payload.walletAddress);
+     setConnected(true);
+    } else {
+     setConnected(false);
+    }
+
+    // Add wallet event listeners
+  if (window.ethereum) {
+    
+      window.ethereum.on('accountsChanged', (accounts) => {
+        if (accounts.length === 0) {
+     
+          setAddress(null);
+          setConnected(false);
+          console.log("Wallet disconnected");
+        } else {
+          
+          setAddress(accounts[0]);
+          setConnected(true);
+         console.log("Wallet switched")
+        }
+      });
+
+    }
+
+  } catch (error) {
+    console.log("Authentication failed:", error);
+  }
+};
+
+
+
+  
